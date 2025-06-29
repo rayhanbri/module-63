376b9c03a423ae5f3c3dd3059650cd7c7912a46e
rayhan <mohammadrayhanx0@gmail.com>
2025-06-29 23:52:41 +0600

    simple login hook from basic

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import image from '../../../assets/authImage.png'
+import { useForm } from 'react-hook-form';
+
 
 const Login = () => {
+    const {register,handleSubmit} = useForm();
+
+    const onSubmit = data =>{
+        console.log(data)
+    }
     return (
         <div>
-          <h1>this is log in</h1>  
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email')} className="input" placeholder="Email" />
+
+                    <label className="label">Password</label>
+                    <input type="password" {...register('password')} className="input" placeholder="Password" />
+
+                    <div><a className="link link-hover">Forgot password?</a></div>
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
