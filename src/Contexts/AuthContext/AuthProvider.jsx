a7812452e3d07c2254fda627370fa6f9486fc01c
rayhan <mohammadrayhanx0@gmail.com>
2025-06-30 01:22:37 +0600

    on auth state change and logout,login,create user added

--- src/Contexts/AuthContext/AuthProvider.jsx
+++ src/Contexts/AuthContext/AuthProvider.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../../firebase/firebase_init';
 
 const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+    const createUser = (email,password) =>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const signIn = (email,password) =>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const logOut = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth ,currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
 
     const authInfo ={
-
+            createUser,
+            signIn,
+            user,
+            loading,
+            logOut
     }
     return (
         <AuthContext value={authInfo}>
