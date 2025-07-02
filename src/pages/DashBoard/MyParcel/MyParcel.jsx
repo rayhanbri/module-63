5f97251b41e78862112558f7de36b1d5477d5a0a
rayhan <mohammadrayhanx0@gmail.com>
2025-07-02 17:04:26 +0600

    use tanstack query

--- src/pages/DashBoard/MyParcel/MyParcel.jsx
+++ src/pages/DashBoard/MyParcel/MyParcel.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import useAuth from '../../../Hooks/useAuth';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const MyParcel = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const { data: parcels=[] } = useQuery({
+        queryKey: ['my-parcel', user.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/parcels?emal=${user.email}`);
+            return res.data
+        }
+    });
+
+    console.log(parcels)
     return (
         <div>
-            This  is my Parcel
+            This  is my Parcel : {parcels.length}
         </div>
     );
 };
