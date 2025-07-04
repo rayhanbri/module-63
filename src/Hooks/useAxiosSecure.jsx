a20588edf7fe41198b0e66dd656daa7de738eb53
rayhan <mohammadrayhanx0@gmail.com>
2025-07-04 23:02:50 +0600

    added jwt token

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react';
+import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
@@ -8,6 +9,17 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const {user} = useAuth()
+    console.log(user.accessToken)
+     // 1.jwt verification start here 
+    axiosSecure.interceptors.request.use(config =>{
+        config.headers.Authorization =`Bearer ${user.accessToken}`
+        //2 ekhon ei ta network tab e dhekhabhe 
+        //3 now go to backend payment e jaw 
+        return config
+    },error =>{
+           return Promise.reject(error);
+    })
     return axiosSecure;
     // eibar eitare send parcel theke call khorbho 
 };
